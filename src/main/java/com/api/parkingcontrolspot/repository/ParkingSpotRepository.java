8569d696c882a8d73b64299b88e8be7a05880674
joaovictorzup <joao.victor@zup.com.br>
2023-01-25 20:21:47 -0300

    Add all project

---
+++ src/main/java/com/api/parkingcontrolspot/repository/ParkingSpotRepository.java
@@ -0,0 +1,17 @@
+package com.api.parkingcontrolspot.repository;
+
+import java.util.UUID;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.api.parkingcontrolspot.model.ParkingSpotModel;
+
+@Repository
+public interface ParkingSpotRepository extends JpaRepository<ParkingSpotModel, UUID> {
+
+    boolean existsByLicensePlateCar(String licensePlateCar);
+    boolean existsByParkingSpotNumber(String parkingSpotNumber);
+    boolean existsByApartmentAndBlock(String apartment, String block);
+
+}
---
+++ src/test/java/com/api/parkingcontrolspot/ParkingSpotControlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.api.parkingcontrolspot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ParkingSpotControlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
